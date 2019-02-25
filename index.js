c96351847a0b165807e2291621a0fa39a0c11d60
surferwat <wmiura@gmail.com>
2019-02-25 11:02:45 -0600

    feat: add modify and delete endpoints

--- index.js
+++ index.js
@@ -81,6 +81,57 @@ router.post('/pointers/:name', auth.authorize, async ctx => {
   ctx.body = pointer
 })
 
+// Update revshare pointer
+router.put('/pointers/:name', auth.authorize, async ctx => {
+  const { payout } = ctx.request.body
+
+  if (/[^A-Za-z0-9\-_]/.test(ctx.params.name)) {
+    ctx.throw(400, 'invalid :name. must only use base64url characters.')
+    return
+  }
+
+  try {
+    const pointer = await db.get('pointer:' + ctx.params.name)
+  } catch (e) {
+    debug('failed to lookup pointer. error=' + e.message)
+    ctx.throw(400, 'entry does not exist')
+  }
+
+  let sum = 0
+  for (const entity of payout) {
+    if (typeof entity.percent !== 'number' || isNaN(entity.percent)) {
+      ctx.throw(400, 'invalid percent. must be number.')
+      return
+    }
+
+    if (entity.percent <= 0) {
+      ctx.throw(400, 'invalid percent. must be positive.')
+    }
+
+    sum += entity.percent
+
+    if (!entity.pointer || typeof entity.pointer !== 'string') {
+      ctx.throw(400, 'invalid pointer. must be string.')
+      return
+    }
+  }
+
+  if (sum !== 100) {
+    ctx.throw(400, 'invalid payout. must percents must sum to 100')
+    return
+  }
+
+  const pointer = {
+    name: ctx.params.name,
+    payout: ctx.request.body.payout
+  }
+
+  await db.put('pointer:' + ctx.params.name, JSON.stringify(pointer))
+
+  debug('updated pointer. name=' + ctx.params.name)
+  ctx.body = pointer
+})
+
 // Get details of revshare pointer
 router.get('/pointers/:name', auth.authorize, async ctx => {
   try {
@@ -88,6 +139,19 @@ router.get('/pointers/:name', auth.authorize, async ctx => {
     ctx.body = JSON.parse(pointerJSON)
   } catch (e) {
     debug('failed to lookup pointer. error=' + e.message)
+  }
+})
+
+// Delete revshare pointer
+router.del('/pointers/:name', auth.authorize, async ctx => {
+  try {
+    const pointer = await db.get('pointer:' + ctx.params.name)
+    if (pointer) {
+      await db.del('pointer:' + ctx.params.name)
+      ctx.body = 'deleted pointer. name=' + ctx.params.name
+    }
+  } catch (e) {
+    debug('failed to delete pointer. error=' + e.message)
   }
 })
 
